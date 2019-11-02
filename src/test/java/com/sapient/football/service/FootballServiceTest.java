02139e73a8159c97c52428ecf30c9b2cebc5a464
hiring <hiring1@WKWIN0609040.global.publicisgroupe.net>
2019-11-02 16:07:03 +0530

    changes in pom file

--- src/test/java/com/sapient/football/service/FootballServiceTest.java
+++ src/test/java/com/sapient/football/service/FootballServiceTest.java
@@ -2,15 +2,47 @@ package com.sapient.football.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.sapient.football.model.TeamStanding;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
 
 @RunWith(SpringRunner.class)
 public class FootballServiceTest {
 	
-	@Autowired
-	FootballService footballService;
+	
+	@InjectMocks
+    private FootballService footballService;
+	
 
+    @Mock
+    private CountryService countryService;
+    
+    @Mock
+    private StandingService standingService;
+    
+    @Mock
+    private LeagueService leagueService;
+	
+
+    @Test
+    public void testOverallPosition() {
+    	TeamStanding teamStandingPosition = new TeamStanding();
+    	teamStandingPosition.setOverallLeaguePosition("1");
+    	when(countryService.getCountryIdByName(any(String.class))).thenReturn("41");
+    	when(leagueService.getLeagueIdByCountryIdAndLeagueName(any(String.class), any(String.class))).thenReturn("148");
+    	when(standingService.getTeamStandingPostion(any(String.class), any(String.class))).thenReturn(teamStandingPosition);
+    	
+    	footballService.findStandingOfATeam("England", "Premier League", "Liverpool")
+    	
+    	
+    }
 	@Test
 	public void standingOfAteamPositionTest() {
 		footballService.findStandingOfATeam("England", "Premier League", "Liverpool");
